ed37b9e92c280feef77fbd1c1c67c9ab874f120e
Nandan093 <62063300+Nandan093@users.noreply.github.com>
2020-03-13 17:53:59 +0530

    Add files via upload

---
+++ Data types conversion.py
@@ -0,0 +1,131 @@
+# Data type conversions
+
+a = 8
+
+b = 7.54
+
+c = "Nandan"
+
+d = True
+
+e = '7'
+
+#Coversion of int to float
+
+f = float(a)
+
+print(type(a))
+
+print(type(f))
+
+#Conversion of int to String
+
+g = str(a)
+
+print(type(a))
+
+print(type(g))
+
+#Conversion of int to Bool
+
+h = bool(a)
+
+print(type(a))
+
+print(type(h))
+
+#Conversion of float to int
+
+i = int(b)
+
+print(type(b))
+
+print(type(i))
+
+#Conversion of float to String
+
+j = str(b)
+
+print(type(b))
+
+print(type(j))
+
+#Conversion of float to Bool
+
+k = bool(b)
+
+print(type(b))
+
+print(type(k))
+
+#Conversion of String to int
+'''
+l = int(c)
+
+print(type(c))
+
+print(type(l))      # Error occured as not able to convert str to int
+
+#Conversion of String to float
+
+m = float(c)
+
+print(type(c))
+
+print(type(m))      # Error occured as not able to convert str to int
+'''
+#Conversion of String to Bool
+
+n = bool(c)
+
+print(type(c))
+
+print(type(n))
+
+#Conversion of Bool to Int
+
+o = int(d)
+
+print(type(d))
+
+print(type(o))
+
+#Conversion of Bool to float
+
+p = float(d)
+
+print(type(d))
+
+print(type(p))
+
+#Conversion of Bool to String
+
+q = str(d)
+
+print(type(d))
+
+print(type(q))
+
+#Conversion of number string to int
+
+r = int(e)
+
+print(type(e))
+
+print(type(r))
+
+#Conversion of number String to float
+
+s = float(e)
+
+print(type(e))
+
+print(type(s))
+
+#Conversion of number String to Bool
+
+t = bool(e)
+
+print(type(e))
+
+print(type(t))
---
+++ Geometry calculations.py
@@ -0,0 +1,38 @@
+r = 9.2
+
+h = 3
+
+pi = 22/7
+
+Volume = int(1/3*pi*r*r*h)
+
+print("Volume of cone= ",Volume)
+
+
+
+
+n = 5
+
+p = 5*12
+
+r = 13/100
+
+Output = float(n * p * r)
+
+print("Output of npr= ",Output)
+
+
+
+r  = 3.2
+
+Output = str(pi * r**2)
+
+print("Area of a circle = ",Output)
+
+
+
+r = 3.8
+
+Output = int(2 * pi * r)
+
+print("Circumference of a circle = ",Output)
