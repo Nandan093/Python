1dc6f4f682ae0042bd3a82a88ab020861945c14b
Nandan <62063300+Nandan093@users.noreply.github.com>
2020-03-27 09:22:01 +0530

    Add files via upload

---
+++ Negative indexing.py
@@ -0,0 +1,35 @@
+a = "computer_science"
+
+print(a[2:-2:2])       #mue_ce
+
+print(a[2:-2:-2])      #empty string
+
+print(a[-2:2:2])       #empty string
+
+print(a[-2:2:-2])      #cec_eu
+
+print(a[:5:1])         #compu
+
+print(a[:5:-1])        #ecneics_re
+
+print(a[10::-1])       #cs_retupmoc
+
+print(a[10::-2])       #c_eumc
+
+print(a[-10:10:10])    #e
+
+print(a[::10])         #cc
+
+print(a[::5])          #ctce
+ 
+print(a[::-5])         #ectc
+
+print(a[::-8])         #er
+
+print(a[0:8:2])        #cmue
+
+print(a[-5:5:2])       #empty string
+
+print(a[-10:-1:-1])    #empty string
+
+print(a[-9:9:-2])      #empty string
